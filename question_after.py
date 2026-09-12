1c6d556605e279a22272b684fe09c1e5f67de494
compiii <jerrylacmou@gmail.com>
2026-09-13 00:41:15 +0200

    Support legacy question metadata generation

--- question_after.py
+++ question_after.py
@@ -4,6 +4,21 @@ Helper to create 'COMPILE_.../session.json' file containing questionnary informa
 """
 import json
 
+# Modern questionnaires register classes named Q... while they are defined.
+# Older questionnaires either use arbitrary class names or expose an init()
+# factory returning question instances. Keep the modern path unchanged and
+# enable these compatibility paths only when no class was registered.
+if not question_classes: # pylint: disable=undefined-variable
+    initializer = globals().get('init')
+    if callable(initializer):
+        Session(initializer()) # pylint: disable=undefined-variable
+
+if not question_classes: # pylint: disable=undefined-variable
+    question_classes.extend(Question.__subclasses__()) # pylint: disable=undefined-variable
+
+if not question_classes: # pylint: disable=undefined-variable
+    raise ValueError('No question found while generating questions.json')
+
 infos = []
 for classe in question_classes: # pylint: disable=undefined-variable
     classe._version = 'a'
